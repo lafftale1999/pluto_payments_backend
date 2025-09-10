3becd03d672f10eae0a2fa4fde49d53b3d780329
Lafftale <lafftale1999@gmail.com>
2025-09-10 23:06:09 +0200

    initial commit

---
+++ src/main/java/com/otulp/pluto_payments_backend/PlutoPaymentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.otulp.pluto_payments_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlutoPaymentsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlutoPaymentsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otulp/pluto_payments_backend/PlutoPaymentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otulp.pluto_payments_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutoPaymentsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
