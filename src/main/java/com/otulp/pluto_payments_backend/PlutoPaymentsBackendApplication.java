f2a1b66604c2f9cbebc78156e29ee83743d55b2b
Lafftale <lafftale1999@gmail.com>
2025-09-17 10:53:22 +0200

    added test controller for hardware requests

--- src/main/java/com/otulp/pluto_payments_backend/PlutoPaymentsBackendApplication.java
+++ src/main/java/com/otulp/pluto_payments_backend/PlutoPaymentsBackendApplication.java
@@ -3,7 +3,12 @@ package com.otulp.pluto_payments_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+        }
+)
 public class PlutoPaymentsBackendApplication {
 
     public static void main(String[] args) {
