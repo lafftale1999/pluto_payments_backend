d9d79643904d23b94387866524dcd2d5cbcaef60
Lafftale <lafftale1999@gmail.com>
2025-09-18 14:06:11 +0200

    commit before leaving

--- src/main/java/com/otulp/pluto_payments_backend/Controllers/PaymentController.java
+++ src/main/java/com/otulp/pluto_payments_backend/Controllers/PaymentController.java
@@ -58,7 +58,7 @@ public class PaymentController {
             // LOGG
             return ResponseEntity.badRequest().body("Not accepted");
         }
-        
+
         return ResponseEntity.ok("Payment received properly");
     }
 }
