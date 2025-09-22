0f75a9c96cdb91d9549d61103ff3a645d82365a9
Zaro <63870402+Zar000@users.noreply.github.com>
2025-09-22 13:53:16 +0200

    get card and get invoice now implemented, tested and it works

--- src/main/java/com/otulp/pluto_payments_backend/Services/AccountService.java
+++ src/main/java/com/otulp/pluto_payments_backend/Services/AccountService.java
@@ -1,13 +1,22 @@
 package com.otulp.pluto_payments_backend.Services;
 
 import com.otulp.pluto_payments_backend.DTOs.AccountDTO;
+import com.otulp.pluto_payments_backend.DTOs.InvoiceDTO;
 import com.otulp.pluto_payments_backend.DTOs.PasswordChangeDTO;
+import com.otulp.pluto_payments_backend.DTOs.TransactionDTO;
 import com.otulp.pluto_payments_backend.Models.Customer;
+import com.otulp.pluto_payments_backend.Models.Invoice;
 import org.springframework.http.ResponseEntity;
 
 public interface AccountService {
     ResponseEntity<String> changePassword(PasswordChangeDTO passwordChangeDTO);
 
     public AccountDTO customerToAccountDTO(Customer customer);
+
+    public ResponseEntity<Object> cardToCardDTO(Long card);
+
+    public TransactionDTO transactionToCardDTO(TransactionDTO transactionDTO);
+
+    public InvoiceDTO invoiceToInvoiceDTO(Long invoiceid, Long id);
 
 }
