686e1b5ea80d4a89144fbbb1970c0aa6bca9733b
Johann Hager <johannbonde@hotmail.se>
2025-09-23 10:47:35 +0200

    Added getFirstName in response

--- src/main/java/com/otulp/pluto_payments_backend/PlutoPaymentsBackendApplication.java
+++ src/main/java/com/otulp/pluto_payments_backend/PlutoPaymentsBackendApplication.java
@@ -16,45 +16,45 @@ public class PlutoPaymentsBackendApplication {
         SpringApplication.run(PlutoPaymentsBackendApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner fill (DeviceRepository repo, CountryRepo countryRepo, AddressRepo addressRepo,
-//                                   CardRepo cardRepo, CustomerRepo customerRepo, InvoiceRepo invoiceRepo,
-//                                   TransactionRepo transactionRepo) {
-//        return (args) -> {
-//            repo.save(new Device(true, "E8:6B:EA:D9:05:E4", "carl_esp32"));
-//            Device device = repo.findByMacAddress("E8:6B:EA:D9:05:E4");
-//
-//            Country country = new Country(null, "Sweden", "+46");
-//            countryRepo.save(country);
-//
-//            Address address = new Address("Artellerigatan 19", "110 32", "Stockholm", country);
-//            addressRepo.save(address);
-//
-//            LocalDate issueDate = LocalDate.of(2023, 4, 12);
-//            LocalDate expiryDate = issueDate.plusYears(3);
-//            Card card = new Card (
-//                    "23 3F AA AC",
-//                    "770c3cbf77615a1d94f88d0f9ed148823de46e4518a3f290fec2859e85b501ef",
-//                    expiryDate, issueDate, true, 0
-//            );
-//
-//            Customer customer = new Customer(
-//                    "Rigbert", "Moggby", 9300f, 20000f, 4560f, "0707837291",
-//                    LocalDate.of(1990, 1, 1),
-//                    "moggingOnYou@gmail.com", "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9",
-//                    Customer.Type.NEW, card, address
-//            );
-//
-//            customerRepo.save(customer);
-//            transactionRepo.save(new TransactionInformation(null, 3240f, LocalDate.now(), device, customer, card));
-//            transactionRepo.save(new TransactionInformation(null, 4930f, LocalDate.now(), device, customer, card));
-//            transactionRepo.save(new TransactionInformation(null, 1130f, LocalDate.now(), device, customer, card));
-//
-//            LocalDate invoiceIssueDate = LocalDate.now();
-//            LocalDate invoiceExpiryDate = invoiceIssueDate.plusMonths(1);
-//            LocalDate invoiceReminderDate = invoiceExpiryDate.plusMonths(1);
-//
-//            invoiceRepo.save(new Invoice(null, customer, 9300f, Invoice.Status.PENDING, invoiceIssueDate, invoiceExpiryDate, invoiceReminderDate));
-//        };
-//    }
+    @Bean
+    public CommandLineRunner fill (DeviceRepository repo, CountryRepo countryRepo, AddressRepo addressRepo,
+                                   CardRepo cardRepo, CustomerRepo customerRepo, InvoiceRepo invoiceRepo,
+                                   TransactionRepo transactionRepo) {
+        return (args) -> {
+            repo.save(new Device(true, "E8:6B:EA:D9:05:E4", "carl_esp32"));
+            Device device = repo.findByMacAddress("E8:6B:EA:D9:05:E4");
+
+            Country country = new Country(null, "Sweden", "+46");
+            countryRepo.save(country);
+
+            Address address = new Address("Artellerigatan 19", "110 32", "Stockholm", country);
+            addressRepo.save(address);
+
+            LocalDate issueDate = LocalDate.of(2023, 4, 12);
+            LocalDate expiryDate = issueDate.plusYears(3);
+            Card card = new Card (
+                    "23 3F AA AC",
+                    "770c3cbf77615a1d94f88d0f9ed148823de46e4518a3f290fec2859e85b501ef",
+                    expiryDate, issueDate, true, 0
+            );
+
+            Customer customer = new Customer(
+                    "Rigbert", "Moggby", 9300f, 20000f, 4560f, "0707837291",
+                    LocalDate.of(1990, 1, 1),
+                    "moggingOnYou@gmail.com", "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9",
+                    Customer.Type.NEW, card, address
+            );
+
+            customerRepo.save(customer);
+            transactionRepo.save(new TransactionInformation(null, 3240f, LocalDate.now(), device, customer, card));
+            transactionRepo.save(new TransactionInformation(null, 4930f, LocalDate.now(), device, customer, card));
+            transactionRepo.save(new TransactionInformation(null, 1130f, LocalDate.now(), device, customer, card));
+
+            LocalDate invoiceIssueDate = LocalDate.now();
+            LocalDate invoiceExpiryDate = invoiceIssueDate.plusMonths(1);
+            LocalDate invoiceReminderDate = invoiceExpiryDate.plusMonths(1);
+
+            invoiceRepo.save(new Invoice(null, customer, 9300f, Invoice.Status.PENDING, invoiceIssueDate, invoiceExpiryDate, invoiceReminderDate));
+        };
+    }
 }
